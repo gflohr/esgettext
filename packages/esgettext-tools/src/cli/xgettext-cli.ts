d73ef0fe66a29f406c53ad52f7664c4e119e8793
Guido Flohr <guido.flohr@cantanea.com>
2020-04-24 18:03:10 +0300

    basic cli options

--- packages/esgettext-tools/src/cli/xgettext-cli.ts
+++ packages/esgettext-tools/src/cli/xgettext-cli.ts
@@ -1,1 +1,17 @@
 import * as yargs from 'yargs';
+
+const y = yargs.option('patterns', {
+	alias: 'p',
+	describe: 'Default patterns',
+	type: 'array',
+	default: ['/**/*.html', '/**/*.js', '/**/*.ts'],
+	hidden: true
+});
+
+const parsed = y.parse();
+export const cli = y
+.usage('Extract strings from files for translation.\nUsage: $0 [options]')
+.version(require(__dirname + '/../../package.json').version)
+.alias('version', 'v')
+.help('help')
+.alias('help', 'h')
