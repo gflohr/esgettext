abddd855fd68075d6dbdb1269f3d6e3f3130917a
Guido Flohr <guido.flohr@cantanea.com>
2020-04-25 19:31:58 +0300

    documentation for setLocale()

--- src/gtx.ts
+++ src/gtx.ts
@@ -7,7 +7,7 @@ let isBrowser = isNode ? false : true;
 let useLocale = 'POSIX';
 const browserLocaleRegex = new RegExp('^[a-z0-9]+(?:-[a-z0-9]+)*', 'i');
 const nodeLocaleRegex = new RegExp(
-	'^([a-z]{2})(?:-([a-z]{2}))?(?:@([a-z]+))?(?:.([-a-z0-9]+))?$',
+	'^([a-z]{2,3})(?:-([a-z]{2}))?(?:@([a-z]+))?(?:.([-a-z0-9]+))?$',
 	'i',
 );
 
@@ -51,7 +51,24 @@ export function useJSON(_json: boolean): void {
 /**
  * Change the locale.
  *
+ * For the web you can all valid language identifier tags that BCP47 allows
+ * (and actually a lot more). The tag is always used unmodified.
+ *
+ * For server environments, the locale identifier has to match the following
+ * scheme:
+ *
+ *   ll-CC@variant.charset
+ *
+ * `ll` is the two- or three-letter language code.
+ * `CC` is the optionl two-letter country code.
+ * `variant` is an optional variant (letters and digits).
+ * `charset` is an optional character set (letters, digits, and the hyphen).
+ *
+ * The language code is always converted to lowercase, the country code is
+ * converted to uppercase, variant and charset are used as is.
+ *
  * @param locale the locale identifier
+ * @returns the locale in use
  */
 export function setLocale(locale: string): string {
 	const ucLocale = locale.toUpperCase();
