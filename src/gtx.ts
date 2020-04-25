5c22c9a2266eae96349d1ddaaf328c95ace91f3e
Guido Flohr <guido.flohr@cantanea.com>
2020-04-25 16:53:10 +0300

    bindtextdomain stub

--- src/gtx.ts
+++ src/gtx.ts
@@ -1,11 +1,8 @@
-// Dummy gettext implementation.
+let useJSONCatalog = true;
+
 interface Placeholder {
 	[index: string]: string
 };
-
-export function _(msgid: string) {
-	return msgid;
-}
 
 function expand(msg: string, placeholders: Placeholder): string {
 	return msg.replace(/\{([a-zA-Z][0-9a-zA-Z]*)\}/g, (_, match) => {
@@ -17,6 +14,45 @@ function expand(msg: string, placeholders: Placeholder): string {
 	});
 }
 
+/**
+ * Set the preferred message format.
+ *
+ * @param json true if json should preferred over mo files.
+ *
+ * The library can either load message catalogs in GtxI18N JSON or
+ * in mo format.
+ */
+export function useJSON(json: boolean) {
+	useJSONCatalog = json;
+}
+
+export function bindtextdomain(domainname: string, path?: string): Promise {
+	return new Promise(resolve => {
+		resolve();
+	});
+}
+
+/**
+ * Translate a simple string.
+ *
+ * @param msgid the string to translate
+ *
+ * @returns the translated string
+ */
+export function _(msgid: string) {
+	return msgid;
+}
+
+/**
+ * Translate a string with placeholders.
+ *
+ * Placeholder names must begin with an ASCII alphabetic character (a-z, A-Z)
+ * and can be followed by an arbitrary number of ASCII alphabetic characters
+ * or ASCII digits (a-z, A-Z).
+ *
+ * @param msgid
+ * @param placeholders
+ */
 export function _x(msgid: string, placeholders: Placeholder): string {
 	return expand(msgid, placeholders);
 }
