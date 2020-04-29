4ac2c49ce1e0a5f02f7f78b9adf2e10ca212cd99
Guido Flohr <guido.flohr@cantanea.com>
2020-04-29 11:52:02 +0300

    s/got/superagent/

--- src/transport/http.ts
+++ src/transport/http.ts
@@ -1,10 +1,11 @@
-import got from 'got';
+import superagent from 'superagent';
 import { Transport } from './transport.interface';
 
 export class TransportHttp implements Transport {
 	loadFile(url: string, _encoding: string): Promise<string> {
 		return new Promise((resolve, reject) => {
-			got(url)
+			superagent
+				.get(url)
 				.then((response: { body: string | PromiseLike<string> }) =>
 					resolve(response.body),
 				)
