755bb1bf91840d522db136db0e2e0f655de1d90d
Guido Flohr <guido.flohr@cantanea.com>
2020-05-01 10:01:32 +0300

    validate catalog without ajv
    This saves about 280 k browser code.

--- packages/esgettext-runtime/src/parser/parse-json-catalog.ts
+++ packages/esgettext-runtime/src/parser/parse-json-catalog.ts
@@ -1,13 +1,35 @@
-import Ajv from 'ajv';
 import { Catalog } from '../core/catalog';
-import * as catalogSchema from '../core/catalog-schema.json';
 
 export function validateJsonCatalog(data: Catalog): Catalog {
-	const ajv = new Ajv();
+	// We could use ajv but it results in almost 300 k minimized code
+	// for the browser bundle. This validator instead is absolutely
+	// minimalistic, and only avoids exceptions that can occur, when
+	// accessing entries.
 
-	if (!ajv.validate(catalogSchema, data)) {
-		//console.log(ajv.errorsText());
-		throw new Error(ajv.errorsText());
+	if (typeof data === 'undefined') {
+		throw new Error('catalog is not defined');
+	}
+
+	if (data.constructor !== Object) {
+		throw new Error('catalog must be a dictionary');
+	}
+
+	// We don't care about major and minor because they are actually not
+	// used.
+
+	const entries = data.entries;
+	if (typeof entries === 'undefined') {
+		throw new Error('catalog.entries is not defined');
+	}
+
+	if (entries.constructor !== Object) {
+		throw new Error('catalog.entries must be a dictionary');
+	}
+
+	for (const [key, value] of Object.entries(entries)) {
+		if (!Array.isArray(value)) {
+			throw new Error(`catalog entry for key '${key}' is not an array`);
+		}
 	}
 
 	return data;
