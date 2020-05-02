7963bca0d03c8d17792e0903a723b1b9cb3890cd
Guido Flohr <guido.flohr@cantanea.com>
2020-05-02 10:01:05 +0300

    test default offsets

--- packages/esgettext-runtime/src/core/data-viewlet.spec.ts
+++ packages/esgettext-runtime/src/core/data-viewlet.spec.ts
@@ -1,5 +1,6 @@
 import { DataViewlet } from './data-viewlet';
 
+// This string has 34 bytes.
 const abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZÄÖÜß';
 const uint8array = new TextEncoder().encode(abc);
 const dv = new DataViewlet(uint8array);
@@ -7,10 +8,16 @@ const dv = new DataViewlet(uint8array);
 describe('bufferling', () => {
 	describe('binary', () => {
 		it('big-endian at offset 0', () => {
-			expect(dv.readUint32BE(0)).toEqual(0x41424344);
+			expect(dv.readUint32BE()).toEqual(0x41424344);
 		});
 		it('little-endian at offset 0', () => {
-			expect(dv.readUint32LE(0)).toEqual(0x44434241);
+			expect(dv.readUint32LE()).toEqual(0x44434241);
+		});
+		it('big-endian past end of buffer', () => {
+			expect(() => dv.readUint32BE(31)).toThrow();
+		});
+		it('little-endian past end of buffer', () => {
+			expect(() => dv.readUint32LE(31)).toThrow();
 		});
 	});
 });
