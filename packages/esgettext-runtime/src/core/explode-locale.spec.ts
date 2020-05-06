67479abc89a82f1db96228b0d564373b03204d6b
Guido Flohr <guido.flohr@cantanea.com>
2020-05-06 11:05:47 +0300

    vary charsets

--- packages/esgettext-runtime/src/core/explode-locale.spec.ts
+++ packages/esgettext-runtime/src/core/explode-locale.spec.ts
@@ -7,16 +7,34 @@ describe('explode locale', () => {
 			const paths = explodeLocale(splitLocale('de'));
 			expect(paths).toEqual([['de']]);
 		});
+		it('de-at', () => {
+			const paths = explodeLocale(splitLocale('de-at'));
+			expect(paths).toEqual([['de-at']]);
+		});
 	});
 
 	describe('server', () => {
 		it('de_DE', () => {
-			const paths = explodeLocale(splitLocale('de_DE'));
+			const paths = explodeLocale(splitLocale('de_DE'), true);
 			expect(paths).toEqual([['de'], ['de_DE']]);
 		});
 		it('de_DE@ksh', () => {
-			const paths = explodeLocale(splitLocale('de_DE@ksh'));
+			const paths = explodeLocale(splitLocale('de_DE@ksh'), true);
 			expect(paths).toEqual([['de@ksh'], ['de_DE@ksh']]);
+		});
+		it('de_DE.UTF-8@ksh', () => {
+			const paths = explodeLocale(splitLocale('de_DE.UTF-8@ksh'), true);
+			expect(paths).toEqual([
+				['de.UTF-8@ksh', 'de@ksh'],
+				['de_DE.UTF-8@ksh', 'de_DE@ksh'],
+			]);
+		});
+		it('de_DE.utf-8@ksh', () => {
+			const paths = explodeLocale(splitLocale('de_DE.utf-8@ksh'), true);
+			expect(paths).toEqual([
+				['de.utf-8@ksh', 'de.UTF-8@ksh', 'de@ksh'],
+				['de_DE.utf-8@ksh', 'de_DE.UTF-8@ksh', 'de_DE@ksh'],
+			]);
 		});
 	});
 });
--- packages/esgettext-runtime/src/core/explode-locale.ts
+++ packages/esgettext-runtime/src/core/explode-locale.ts
@@ -2,16 +2,37 @@ import { SplitLocale } from './split-locale';
 
 type ExplodedLocale = Array<Array<string>>;
 
-export function explodeLocale(locale: SplitLocale): ExplodedLocale {
+export function explodeLocale(
+	locale: SplitLocale,
+	vary?: boolean,
+): ExplodedLocale {
 	const retval: ExplodedLocale = [];
 	const lsep = locale.underscoreSeparator ? '_' : '-';
+	let i = vary ? 0 : locale.tags.length - 1;
+	const hasCharset = typeof locale.charset !== 'undefined';
+	const hasModifier = typeof locale.modifier !== 'undefined';
 
-	for (let i = 0; i < locale.tags.length; ++i) {
-		let lingua = locale.tags.slice(0, i + 1).join(lsep);
-		if (typeof locale.modifier !== 'undefined') {
-			lingua += '@' + locale.modifier;
+	const charsets = hasCharset ? [locale.charset] : [''];
+	if (vary && hasCharset) {
+		const ucCharset = locale.charset.toUpperCase();
+		if (ucCharset !== locale.charset) {
+			charsets.push(ucCharset);
 		}
-		retval.push([lingua]);
+		charsets.push('');
+	}
+
+	for (; i < locale.tags.length; ++i) {
+		const lingua = locale.tags.slice(0, i + 1).join(lsep);
+		const ids = new Array<string>();
+
+		charsets.forEach((charset) => {
+			let id = charset.length ? lingua + '.' + charset : lingua;
+			if (hasModifier) {
+				id += '@' + locale.modifier;
+			}
+			ids.push(id);
+		});
+		retval.push(ids);
 	}
 
 	return retval;
