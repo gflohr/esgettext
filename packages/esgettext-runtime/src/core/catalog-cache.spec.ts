ba8ba3ad17264f73fc6677e369dfaf7ad9d42efc
Guido Flohr <guido.flohr@cantanea.com>
2020-05-06 17:36:14 +0300

    test catalog cache

--- packages/esgettext-runtime/src/core/catalog-cache.spec.ts
+++ packages/esgettext-runtime/src/core/catalog-cache.spec.ts
@@ -1,4 +1,6 @@
 import { CatalogCache } from './catalog-cache';
+import { germanicPlural } from './germanic-plural';
+import { Catalog } from './catalog';
 
 describe('catalog-cache', () => {
 	const cache = CatalogCache.getInstance();
@@ -6,6 +8,17 @@ describe('catalog-cache', () => {
 	describe('singleton', () => {
 		it('should be a singleton', () => {
 			expect(CatalogCache.getInstance() === cache).toBeTruthy();
+		});
+		it('should return null for non-existing catalogs', () => {
+			const catalog: Catalog = {
+				major: 0,
+				minor: 0,
+				pluralFunction: germanicPlural,
+				entries: {},
+			};
+			CatalogCache.store('xy', 'once', catalog);
+			expect(CatalogCache.lookup('xy', 'once')).toBeDefined();
+			expect(CatalogCache.lookup('xy', 'twice')).toBeNull();
 		});
 	});
 });
