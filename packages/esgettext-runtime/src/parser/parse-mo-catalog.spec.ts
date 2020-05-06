631bd6fa96e431afea542a41942d351c7fb50b03
Guido Flohr <guido.flohr@cantanea.com>
2020-05-06 17:55:49 +0300

    test little-endian mo files

--- packages/esgettext-runtime/src/parser/parse-mo-catalog.spec.ts
+++ packages/esgettext-runtime/src/parser/parse-mo-catalog.spec.ts
@@ -1,61 +1,18 @@
-import { readFile } from 'fs';
-import { promisify } from 'util';
-import { Catalog } from '../core/catalog';
+import { TransportFs } from '../transport';
 import { parseMoCatalog } from './parse-mo-catalog';
 
-describe('parse MO', () => {
-	describe('mytest.mo parsed', () => {
-		let catalog: Catalog;
-		const filename = 'src/assets/locale/de/LC_MESSAGES/mytest.mo';
+const transport = new TransportFs();
 
-		beforeAll(async () => {
-			const raw = await promisify(readFile)(filename);
-			catalog = parseMoCatalog(raw);
-		});
-
-		it('should not be null', () => {
-			expect(catalog).not.toBeNull();
-		});
-
-		it('should have be of major revision 0', () => {
-			expect(catalog.major).toEqual(0);
-		});
-
-		it('should have be of minor revision 0', () => {
-			expect(catalog.minor).toEqual(0);
-		});
-
-		it('should have a translation for "Open ..." with context', () => {
-			const trans = ['Öffnen ...'];
-			expect(catalog.entries['Menu\u0004Open ...']).toEqual(trans);
-		});
-
-		it('should have two translations for "Hello, world!"', () => {
-			const trans = ['Hallo, Welt!', 'Hallo, Welten!'];
-			expect(catalog.entries['Hello, world!']).toEqual(trans);
-		});
-
-		it('should have two translations for "Hello, world!" with context', () => {
-			const trans = ['Hallo, Welt!', 'Hallo, Welten!'];
-			expect(catalog.entries['duplicate\u0004Hello, world!']).toEqual(trans);
-		});
-	});
-
-	describe('revision1.mo parsed', () => {
-		const filename = 'src/assets/locale/de/LC_MESSAGES/revision1.mo';
-
-		it('should throw an exception', async () => {
-			const raw = await promisify(readFile)(filename);
-			expect(() => parseMoCatalog(raw)).toThrow('unsupported major revision 1');
-		});
-	});
-
-	describe('de.po parsed', () => {
-		const filename = 'src/assets/locale/de/LC_MESSAGES/mytest.po';
-
-		it('should throw an exception', async () => {
-			const raw = await promisify(readFile)(filename);
-			expect(() => parseMoCatalog(raw)).toThrow('mo file corrupted');
-		});
+describe('MO catalogs', () => {
+	it('should read little-endian catalogs', async () => {
+		return transport
+			.loadFile('src/assets/locale/de_AT/LC_MESSAGES/additional.mo')
+			.then((buffer) => {
+				const catalog = parseMoCatalog(Buffer.from(buffer));
+				expect(catalog).toBeDefined();
+				expect(catalog.entries).toBeDefined();
+				expect(catalog.entries['']).toBeDefined();
+				expect(catalog.entries['Saturday']).toEqual(['Sonnabend']);
+			});
 	});
 });
