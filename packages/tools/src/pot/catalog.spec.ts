9471852c6fad1724c47b38fea9d56fa7fdc7d4d8
Guido Flohr <guido.flohr@cantanea.com>
2020-05-08 13:33:00 +0300

    make  catalog creation dates deterministic

--- packages/tools/src/pot/catalog.spec.ts
+++ packages/tools/src/pot/catalog.spec.ts
@@ -1,9 +1,11 @@
 import { Catalog } from './catalog';
 
+const date = '2020-04-23 08:50+0300';
+
 describe('translation catalog', () => {
 	describe('initialization', () => {
 		it('should be initialized with zero configuration', () => {
-			const defaultCatalog = new Catalog();
+			const defaultCatalog = new Catalog({ date });
 
 			expect(defaultCatalog.toString()).toMatchSnapshot();
 		});
