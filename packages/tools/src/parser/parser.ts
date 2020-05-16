bb7c022323f9a1cf7806022fe99945631fb73968
Guido Flohr <guido.flohr@cantanea.com>
2020-05-16 09:54:40 +0300

    added encoding to signature

--- packages/tools/src/parser/parser.ts
+++ packages/tools/src/parser/parser.ts
@@ -2,9 +2,9 @@ import { readFileSync } from 'fs';
 import { Catalog } from '../pot/catalog';
 
 export abstract class Parser {
-	abstract parse(input: Buffer, filename: string): Catalog;
+	abstract parse(input: Buffer, filename: string, encoding?: string): Catalog;
 
-	parseFile(filename: string): Catalog {
-		return this.parse(readFileSync(filename), filename);
+	parseFile(filename: string, encoding?: string): Catalog {
+		return this.parse(readFileSync(filename), filename, encoding);
 	}
 }
