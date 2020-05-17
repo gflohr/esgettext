8446f195cf17dbf858cb44c65ae80ad6e18b9505
Guido Flohr <guido.flohr@cantanea.com>
2020-05-17 22:27:30 +0300

    extract all comments

--- packages/tools/src/parser/javascript.spec.ts
+++ packages/tools/src/parser/javascript.spec.ts
@@ -22,5 +22,36 @@ _x('Hello, {name}!');
 			p.parse(Buffer.from(code), 'example.ts');
 			expect(catalog.toString()).toMatchSnapshot();
 		});
+
+		it('should extract all kinds of comments', () => {
+			const catalog = new Catalog({ date });
+			const warner = jest.fn();
+			const p = new JavaScriptParser(catalog, warner);
+			const code = `
+// Single-line comment.
+_('single line');
+
+/* Block comment. */
+_('block');
+
+// Single-line
+/* and block combined. */
+_('single + block');
+
+/* Block */
+// and single-line combined.
+_('block + single');
+
+// Skip this.
+
+// But catch this.
+_('only immediately preceding');
+
+// Only for one string.
+_('catcher'); _('loser');
+`;
+			p.parse(Buffer.from(code), 'example.ts');
+			expect(catalog.toString()).toMatchSnapshot();
+		});
 	});
 });
--- packages/tools/src/parser/parser.ts
+++ packages/tools/src/parser/parser.ts
@@ -1,5 +1,10 @@
 import { readFileSync } from 'fs';
-import { File, StringLiteral, SourceLocation } from '@babel/types';
+import {
+	File,
+	StringLiteral,
+	SourceLocation,
+	CommentBlock,
+} from '@babel/types';
 import traverse, { NodePath } from '@babel/traverse';
 
 import { Catalog } from '../pot/catalog';
@@ -18,12 +23,14 @@ export abstract class Parser {
 	}
 
 	protected extractAllStrings(ast: File): void {
+		const comments = ast.comments as Array<CommentBlock>;
+
 		traverse(ast, {
 			StringLiteral: (path) => {
 				const loc = path.node.loc;
 				const str = this.extractString(path);
 				if (str !== null) {
-					this.addEntry(str, loc);
+					this.addEntry(str, loc, comments);
 				}
 			},
 			DirectiveLiteral: (path) => {
@@ -32,7 +39,7 @@ export abstract class Parser {
 					(path as unknown) as NodePath<StringLiteral>,
 				);
 				if (str !== null) {
-					this.addEntry(str, loc);
+					this.addEntry(str, loc, comments);
 				}
 			},
 		});
@@ -45,6 +52,7 @@ export abstract class Parser {
 	private addEntry(
 		msgid: string,
 		loc: SourceLocation,
+		remainingComments: Array<CommentBlock>,
 		msgidPlural?: string,
 	): void {
 		let flags;
@@ -58,13 +66,63 @@ export abstract class Parser {
 		};
 		const references = [`${dict.filename}:${loc.start.line}`];
 
+		const commentBlocks = this.findPrecedingComments(remainingComments, loc);
+		const extractedComments = commentBlocks.map((block) => block.value.trim());
+
 		this.catalog.addEntry(
 			new POTEntry({
 				msgid,
 				msgidPlural,
 				flags,
 				references,
+				extractedComments,
 			}),
 		);
+	}
+
+	private findPrecedingComments(
+		comments: Array<CommentBlock>,
+		loc: SourceLocation,
+	): Array<CommentBlock> {
+		let last;
+
+		// Find the last relevant comment, which is the first one that
+		// immediately precedes the location.
+		for (last = 0; last < comments.length; ++last) {
+			const commentLocation = comments[last].loc;
+			if (
+				commentLocation.end.line === loc.start.line ||
+				commentLocation.end.line === loc.start.line - 1
+			) {
+				break;
+			} else if (commentLocation.end.line > loc.start.line) {
+				comments.splice(0, last);
+				return [];
+			}
+		}
+
+		if (last >= comments.length) {
+			comments.splice(0, comments.length);
+			return [];
+		}
+
+		// Now go back and find all adjacent comments.
+		let ptr = comments[last].loc;
+		const preceding = comments.splice(0, last + 1);
+
+		if (!last) {
+			return preceding;
+		}
+
+		let first;
+		for (first = preceding.length - 2; first >= 0; --first) {
+			const commentLocation = preceding[first].loc;
+			if (commentLocation.end.line < ptr.start.line - 1) {
+				break;
+			}
+			ptr = commentLocation;
+		}
+
+		return preceding.slice(first + 1);
 	}
 }
