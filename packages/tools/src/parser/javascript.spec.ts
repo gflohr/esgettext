a6f366cbf6ad7d3f013e7acb3f5bbddaa0dfa21e
Guido Flohr <guido.flohr@cantanea.com>
2020-05-17 15:30:13 +0300

    parse empty files

--- packages/tools/src/parser/javascript.spec.ts
+++ packages/tools/src/parser/javascript.spec.ts
@@ -1,7 +1,15 @@
 import { JavaScriptParser } from './javascript';
 
 describe('JavaScript parser', () => {
-	it('should load', () => {
-		expect(new JavaScriptParser().fortyTwo()).toEqual(42);
+	describe('simple code', () => {
+		it('should create an empty catalog', () => {
+			const warner = jest.fn();
+			const p = new JavaScriptParser(warner);
+
+			const code = '';
+			const input = Buffer.from(code);
+
+			expect(p.parse(input, 'example.ts').toString()).toEqual('');
+		});
 	});
 });
--- packages/tools/src/parser/javascript.ts
+++ packages/tools/src/parser/javascript.ts
@@ -1,5 +1,17 @@
-export class JavaScriptParser {
-	fortyTwo(): number {
-		return 42;
+import { Catalog } from '../pot/catalog';
+import { Parser } from './parser';
+
+export class JavaScriptParser extends Parser {
+	private catalog: Catalog;
+
+	constructor(private readonly warner: (msg: string) => void) {
+		super();
+	}
+
+	parse(buf: Buffer, filename: string, encoding?: string): Catalog {
+		// Reset.
+		this.catalog = new Catalog({ fromCode: encoding, noHeader: true });
+
+		return this.catalog;
 	}
 }
