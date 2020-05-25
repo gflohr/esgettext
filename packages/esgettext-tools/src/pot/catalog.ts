1e4d2eb098b1892c3e8ed7af8a019fe548b2f622
Guido Flohr <guido.flohr@cantanea.com>
2020-05-25 12:19:36 +0300

    space after language header field

--- packages/esgettext-tools/src/pot/catalog.ts
+++ packages/esgettext-tools/src/pot/catalog.ts
@@ -87,7 +87,7 @@ POT-Creation-Date: ${properties.date}
 PO-Revision-Date: YEAR-MO-DA HO:MI+ZONE
 Last-Translator: FULL NAME <EMAIL@ADDRESS>
 Language-Team: LANGUAGE <LL@li.org>
-Language:
+Language:\u0020
 MIME-Version: 1.0
 Content-Type: text/plain; charset=CHARSET
 Content-Transfer-Encoding: 8bit
