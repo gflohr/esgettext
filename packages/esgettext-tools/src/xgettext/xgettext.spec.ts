e841cebf1e2487d783eaa82590cf83a6a9962401
Guido Flohr <guido.flohr@cantanea.com>
2020-05-27 10:39:04 +0300

    re-organized xgettext test suite

--- packages/esgettext-tools/src/xgettext/xgettext.spec.ts
+++ packages/esgettext-tools/src/xgettext/xgettext.spec.ts
@@ -251,167 +251,173 @@ msgstr ""
 			);
 		});
 	});
-
-	describe('option --language', () => {
-		afterEach(() => {
-			resetMocks();
-		});
-
-		it('should honor the --language option', () => {
-			const code = `# SOME DESCRIPTIVE TITLE
-# Copyright (C) YEAR THE PACKAGE'S COPYRIGHT HOLDER
-# This file is distributed under the same license as the PACKAGE package.
-# FIRST AUTHOR <EMAIL@ADDRESS>, YEAR.
-#
-#, fuzzy
-msgid ""
-msgstr ""
-"Project-Id-Version: PACKAGE VERSION\\n"
-"Report-Msgid-Bugs-To: MSGID_BUGS_ADDRESS\\n"
-"POT-Creation-Date: 2020-05-25 11:50+0300\\n"
-"PO-Revision-Date: YEAR-MO-DA HO:MI+ZONE\\n"
-"Last-Translator: FULL NAME <EMAIL@ADDRESS>\\n"
-"Language-Team: LANGUAGE <LL@li.org>\\n"
-"Language: \\n"
-"MIME-Version: 1.0\\n"
-"Content-Type: text/plain; charset=utf-8\\n"
-"Content-Transfer-Encoding: 8bit\\n"
-
-#: src/cli/getopt.ts:122
-#, perl-brace-format
-msgid "'{programName}': unrecognized option '--{option}'"
-msgstr ""
-`;
-
-			readFileSync.mockReturnValueOnce(Buffer.from(code));
-
-			const argv = {
-				...baseArgv,
-				// This is on purpose the wrong language.
-				language: 'javascript',
-				_: ['hello6.pot'],
-			};
-			const xgettext = new XGettext(argv, date);
-			expect(xgettext.run()).toEqual(1);
-			//expect(writeFileSync).toHaveBeenCalledTimes(0);
-			expect(warnSpy).not.toHaveBeenCalled();
-			expect(errorSpy).toHaveBeenCalled();
-		});
-
-		it('should bail out on unknown languages', () => {
-			const code = `gtx._('Hello, world!)`;
-
-			readFileSync.mockReturnValueOnce(Buffer.from(code));
-
-			const argv = {
-				...baseArgv,
-				language: 'VBScript',
-				_: ['hello7.js'],
-			};
-			expect(() => new XGettext(argv, date)).toThrow(
-				'language "VBScript" unknown',
-			);
-		});
-
-		it('should accept the language typescript', () => {
-			const code = 'gtx._("Hello, world!");';
-
-			readFileSync.mockReturnValueOnce(Buffer.from(code));
-
-			const argv = {
-				...baseArgv,
-				language: 'TypeScript',
-				_: ['hello-language-typescript.ts'],
-			};
-			const xgettext = new XGettext(argv, date);
-			expect(xgettext.run()).toEqual(0);
-			expect(writeFileSync).toHaveBeenCalledTimes(1);
-			expect(writeFileSync.mock.calls[0][1]).toMatchSnapshot();
-			expect(warnSpy).not.toHaveBeenCalled();
-			expect(errorSpy).not.toHaveBeenCalled();
-		});
-	});
 });
 
-describe('options', () => {
-	describe('option --output', () => {
-		beforeEach(() => {
-			resetMocks();
-		});
+describe('xgettext command-line options', () => {
+	describe('output file location', () => {
+		describe('option --output', () => {
+			beforeEach(() => {
+				resetMocks();
+			});
 
-		it('should honor the option --output', () => {
-			const code = 'console.log(gtx._("Hello, world!"))';
-			readFileSync.mockReturnValueOnce(Buffer.from(code));
-			const argv = {
-				...baseArgv,
-				output: 'option-output.pot',
-				_: ['option-output.js'],
-			};
-			const xgettext = new XGettext(argv, date);
-			expect(xgettext.run()).toEqual(0);
-			expect(writeFileSync).toHaveBeenCalledTimes(1);
-			expect(writeFileSync.mock.calls[0][0]).toEqual('option-output.pot');
-			expect(warnSpy).not.toHaveBeenCalled();
-			expect(errorSpy).not.toHaveBeenCalled();
-		});
+			it('should honor the option --output', () => {
+				const code = 'console.log(gtx._("Hello, world!"))';
+				readFileSync.mockReturnValueOnce(Buffer.from(code));
+				const argv = {
+					...baseArgv,
+					output: 'option-output.pot',
+					_: ['option-output.js'],
+				};
+				const xgettext = new XGettext(argv, date);
+				expect(xgettext.run()).toEqual(0);
+				expect(writeFileSync).toHaveBeenCalledTimes(1);
+				expect(writeFileSync.mock.calls[0][0]).toEqual('option-output.pot');
+				expect(warnSpy).not.toHaveBeenCalled();
+				expect(errorSpy).not.toHaveBeenCalled();
+			});
 
-		it('should write to stdout', () => {
-			const code = 'console.log(gtx._("Hello, world!"))';
-			readFileSync.mockReturnValueOnce(Buffer.from(code));
-			const argv = {
-				...baseArgv,
-				output: '-',
-				_: ['option-output.js'],
-			};
-			const xgettext = new XGettext(argv, date);
+			it('should write to stdout', () => {
+				const code = 'console.log(gtx._("Hello, world!"))';
+				readFileSync.mockReturnValueOnce(Buffer.from(code));
+				const argv = {
+					...baseArgv,
+					output: '-',
+					_: ['option-output.js'],
+				};
+				const xgettext = new XGettext(argv, date);
 
-			const stdoutSpy = jest
-				.spyOn(global.process.stdout, 'write')
-				.mockImplementation(() => {
-					return true;
-				});
-			expect(xgettext.run()).toEqual(0);
-			expect(writeFileSync).toHaveBeenCalledTimes(0);
-			expect(stdoutSpy.mock.calls).toMatchSnapshot();
-			stdoutSpy.mockRestore();
-			expect(warnSpy).not.toHaveBeenCalled();
-			expect(errorSpy).not.toHaveBeenCalled();
+				const stdoutSpy = jest
+					.spyOn(global.process.stdout, 'write')
+					.mockImplementation(() => {
+						return true;
+					});
+				expect(xgettext.run()).toEqual(0);
+				expect(writeFileSync).toHaveBeenCalledTimes(0);
+				expect(stdoutSpy.mock.calls).toMatchSnapshot();
+				stdoutSpy.mockRestore();
+				expect(warnSpy).not.toHaveBeenCalled();
+				expect(errorSpy).not.toHaveBeenCalled();
+			});
 		});
 	});
 
-	describe('option --force-po', () => {
-		beforeEach(() => {
-			resetMocks();
-		});
+	describe('choice of input language', () => {
+		describe('option --language', () => {
+			afterEach(() => {
+				resetMocks();
+			});
 
-		it('should not write empty catalogs', () => {
-			const code = 'console.log("Hello, world!")';
-			readFileSync.mockReturnValueOnce(Buffer.from(code));
-			const argv = {
-				...baseArgv,
-				_: ['force-po1.js'],
-			};
-			const xgettext = new XGettext(argv, date);
-			expect(xgettext.run()).toEqual(0);
-			expect(writeFileSync).not.toHaveBeenCalled();
-			expect(warnSpy).not.toHaveBeenCalled();
-			expect(errorSpy).not.toHaveBeenCalled();
-		});
+			it('should honor the --language option', () => {
+				const code = `# SOME DESCRIPTIVE TITLE
+	# Copyright (C) YEAR THE PACKAGE'S COPYRIGHT HOLDER
+	# This file is distributed under the same license as the PACKAGE package.
+	# FIRST AUTHOR <EMAIL@ADDRESS>, YEAR.
+	#
+	#, fuzzy
+	msgid ""
+	msgstr ""
+	"Project-Id-Version: PACKAGE VERSION\\n"
+	"Report-Msgid-Bugs-To: MSGID_BUGS_ADDRESS\\n"
+	"POT-Creation-Date: 2020-05-25 11:50+0300\\n"
+	"PO-Revision-Date: YEAR-MO-DA HO:MI+ZONE\\n"
+	"Last-Translator: FULL NAME <EMAIL@ADDRESS>\\n"
+	"Language-Team: LANGUAGE <LL@li.org>\\n"
+	"Language: \\n"
+	"MIME-Version: 1.0\\n"
+	"Content-Type: text/plain; charset=utf-8\\n"
+	"Content-Transfer-Encoding: 8bit\\n"
 
-		it('should write empty catalogs with option --force-po', () => {
-			const code = 'console.log("Hello, world!")';
-			readFileSync.mockReturnValueOnce(Buffer.from(code));
-			const argv = {
-				...baseArgv,
-				forcePo: true,
-				_: ['force-po1.js'],
-			};
-			const xgettext = new XGettext(argv, date);
-			expect(xgettext.run()).toEqual(0);
-			expect(writeFileSync).toHaveBeenCalledTimes(1);
-			expect(writeFileSync.mock.calls[0][1]).toMatchSnapshot();
-			expect(warnSpy).not.toHaveBeenCalled();
-			expect(errorSpy).not.toHaveBeenCalled();
+	#: src/cli/getopt.ts:122
+	#, perl-brace-format
+	msgid "'{programName}': unrecognized option '--{option}'"
+	msgstr ""
+	`;
+
+				readFileSync.mockReturnValueOnce(Buffer.from(code));
+
+				const argv = {
+					...baseArgv,
+					// This is on purpose the wrong language.
+					language: 'javascript',
+					_: ['hello6.pot'],
+				};
+				const xgettext = new XGettext(argv, date);
+				expect(xgettext.run()).toEqual(1);
+				//expect(writeFileSync).toHaveBeenCalledTimes(0);
+				expect(warnSpy).not.toHaveBeenCalled();
+				expect(errorSpy).toHaveBeenCalled();
+			});
+
+			it('should bail out on unknown languages', () => {
+				const code = `gtx._('Hello, world!)`;
+
+				readFileSync.mockReturnValueOnce(Buffer.from(code));
+
+				const argv = {
+					...baseArgv,
+					language: 'VBScript',
+					_: ['hello7.js'],
+				};
+				expect(() => new XGettext(argv, date)).toThrow(
+					'language "VBScript" unknown',
+				);
+			});
+
+			it('should accept the language typescript', () => {
+				const code = 'gtx._("Hello, world!");';
+
+				readFileSync.mockReturnValueOnce(Buffer.from(code));
+
+				const argv = {
+					...baseArgv,
+					language: 'TypeScript',
+					_: ['hello-language-typescript.ts'],
+				};
+				const xgettext = new XGettext(argv, date);
+				expect(xgettext.run()).toEqual(0);
+				expect(writeFileSync).toHaveBeenCalledTimes(1);
+				expect(writeFileSync.mock.calls[0][1]).toMatchSnapshot();
+				expect(warnSpy).not.toHaveBeenCalled();
+				expect(errorSpy).not.toHaveBeenCalled();
+			});
+		});
+	});
+
+	describe('output details', () => {
+		describe('option --force-po', () => {
+			beforeEach(() => {
+				resetMocks();
+			});
+
+			it('should not write empty catalogs', () => {
+				const code = 'console.log("Hello, world!")';
+				readFileSync.mockReturnValueOnce(Buffer.from(code));
+				const argv = {
+					...baseArgv,
+					_: ['force-po1.js'],
+				};
+				const xgettext = new XGettext(argv, date);
+				expect(xgettext.run()).toEqual(0);
+				expect(writeFileSync).not.toHaveBeenCalled();
+				expect(warnSpy).not.toHaveBeenCalled();
+				expect(errorSpy).not.toHaveBeenCalled();
+			});
+
+			it('should write empty catalogs with option --force-po', () => {
+				const code = 'console.log("Hello, world!")';
+				readFileSync.mockReturnValueOnce(Buffer.from(code));
+				const argv = {
+					...baseArgv,
+					forcePo: true,
+					_: ['force-po1.js'],
+				};
+				const xgettext = new XGettext(argv, date);
+				expect(xgettext.run()).toEqual(0);
+				expect(writeFileSync).toHaveBeenCalledTimes(1);
+				expect(writeFileSync.mock.calls[0][1]).toMatchSnapshot();
+				expect(warnSpy).not.toHaveBeenCalled();
+				expect(errorSpy).not.toHaveBeenCalled();
+			});
 		});
 	});
 });
