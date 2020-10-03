f6c93d1661f8a02dd2e94d8b82761ef46c1e359f
Guido Flohr <guido.flohr@cantanea.com>
2020-10-03 13:05:17 +0300

    gettextized

--- packages/sample-calendar-library/src/calendar-strings.ts
+++ packages/sample-calendar-library/src/calendar-strings.ts
@@ -1,24 +1,28 @@
+import { Textdomain } from '@esgettext/runtime';
+
+const gtx = Textdomain.getInstance('com.cantanea.sample-calendar-library');
+
 export class CalendarStrings {
 	public getMonth(id: number): string {
 		const months = [
-			'January',
-			'February',
-			'March',
-			'April',
-			'May',
-			'June',
-			'July',
-			'August',
-			'September',
-			'October',
-			'November',
-			'December'
+			Textdomain.N_('January'),
+			Textdomain.N_('February'),
+			Textdomain.N_('March'),
+			Textdomain.N_('April'),
+			Textdomain.N_('May'),
+			Textdomain.N_('June'),
+			Textdomain.N_('July'),
+			Textdomain.N_('August'),
+			Textdomain.N_('September'),
+			Textdomain.N_('October'),
+			Textdomain.N_('November'),
+			Textdomain.N_('December')
 		];
 
 		if (id < 0 || id > months.length) {
 			return null;
 		}
 
-		return months[id];
+		return gtx._(months[id]);
 	}
 }
