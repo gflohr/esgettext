b2e0e4d197463d4c9733f331beae3ba0096cc432
Guido Flohr <guido.flohr@cantanea.com>
2024-01-26 19:28:05 +0200

    check that coverage report exists

--- merge-lcov.js
+++ merge-lcov.js
@@ -7,11 +7,15 @@ const dirs = process.argv.slice(2);
 let output = '';
 dirs.forEach(dir => {
 	const filename = `${dir}/coverage/lcov.info`;
-	const orig = fs.readFileSync(filename, 'utf-8');
-	const patched = orig.replace(/^SF:/gm, () => {
-		return `SF:${dir}/`;
-	});
-	output += patched;
+	// Yes, it is stupid to check whether a file exists.  But some of our
+	// packages simply do not have coverage data.
+	if (fs.existsSync(filename)) {
+		const orig = fs.readFileSync(filename, 'utf-8');
+		const patched = orig.replace(/^SF:/gm, () => {
+			return `SF:${dir}/`;
+		});
+		output += patched;
+	}
 });
 
 try {
