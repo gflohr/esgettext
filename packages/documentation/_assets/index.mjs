b0cd30733cedd702a403959a8aafdc44e8122da4
Guido Flohr <guido.flohr@cantanea.com>
2024-01-27 13:26:23 +0200

    include prismjs

--- packages/documentation/_assets/index.mjs
+++ packages/documentation/_assets/index.mjs
@@ -1,10 +1,19 @@
 // Main entry point.
 
-// First load CSS/SCSS.
-import './scss/pretty-docs/styles.scss';
 import './scss/bootstrap.scss';
-
-// JavaScript.
 import '@popperjs/core';
 import 'bootstrap';
+
 import 'gumshoejs';
+
+import 'prismjs/components/prism-bash.js';
+import 'prismjs/components/prism-css.js';
+import 'prismjs/components/prism-javascript.js';
+import 'prismjs/components/prism-json.js';
+import 'prismjs/components/prism-typescript.js';
+import 'prismjs/plugins/line-numbers/prism-line-numbers.js';
+import 'prismjs/themes/prism.css';
+import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
+
+// PrettyDocs Theme.
+import './scss/pretty-docs/styles.scss';
