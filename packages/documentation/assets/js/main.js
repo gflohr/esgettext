15d074f265a89bce7c3f5acc9906a37b52eb021c
Guido Flohr <guido.flohr@cantanea.com>
2024-01-27 13:32:54 +0200

    remove stickyfill, obsolete by now

--- packages/documentation/assets/js/main.js
+++ packages/documentation/assets/js/main.js
@@ -1,13 +1,5 @@
 "use strict";
 
-
-/* ===== Stickyfill ===== */
-/* Ref: https://github.com/wilddeer/stickyfill */
-// Add browser support to position: sticky
-
-var elements = document.querySelectorAll('.sticky');
-Stickyfill.add(elements);
-
 /* ===== Gumshoe SrollSpy ===== */
 /* Ref: https://github.com/cferdinandi/gumshoe  */
 // Initialize Gumshoe
