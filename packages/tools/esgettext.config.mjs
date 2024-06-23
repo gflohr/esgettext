cbb8697493a207c9bd7a08a09ea8675a88e93c57
Guido Flohr <guido.flohr@cantanea.com>
2024-06-23 17:53:39 +0300

    restore locale list

--- packages/tools/esgettext.config.mjs
+++ packages/tools/esgettext.config.mjs
@@ -1,9 +1,15 @@
+// Configuration for esgettext, created by esgettext v1.3.1.
 export default {
 	package: {
+		name: '@esgettext/tools',
+		version: '1.3.1',
 		textdomain: 'com.cantanea.esgettext-tools',
+		'msgid-bugs-address': 'https://github.com/gflohr/esgettext/issues',
 	},
-	po: {
-		directory: 'po',
-		locales: ['de', 'en-AU', 'en-CA', 'en-GB', 'en-NZ' ],
+	po: { directory: 'po', locales: ['de', 'en-AU', 'en-CA', 'en-GB', 'en-NZ'] },
+	install: { directory: 'src/locale' },
+	programs: {
+		msgmerge: { options: ['verbose'] },
+		msgfmt: { options: ['check', 'statistics', 'verbose'] },
 	},
 };
