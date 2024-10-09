7a8a93a1470c61e9133fd5e4b632dd36d3474172
Guido Flohr <guido.flohr@cantanea.com>
2024-10-09 13:14:24 +0300

    create Buffer from ArrayBuffer

--- packages/runtime/src/transport/fs.ts
+++ packages/runtime/src/transport/fs.ts
@@ -1,12 +1,13 @@
-import { readFile } from 'fs';
+import { promises as fs } from 'fs';
 import { Transport } from './transport.interface';
 
 export class TransportFs implements Transport {
-	loadFile(url: string): Promise<ArrayBuffer> {
-		return new Promise((resolve, reject) => {
-			readFile(url, (err, data) => {
-				return err ? reject(err) : resolve(data);
-			});
-		});
+	async loadFile(filename: string): Promise<ArrayBuffer> {
+		const data = await fs.readFile(filename);
+
+		return data.buffer.slice(
+			data.byteOffset,
+			data.byteOffset + data.byteLength,
+		) as ArrayBuffer;
 	}
 }
