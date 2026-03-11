9c8a0c51d8fbe9d318854ffe6b20015e319925b5
Guido Flohr <guido.flohr@cantanea.com>
2026-03-11 13:47:52 +0200

    do not use MS-DOS path separator

--- packages/tools/src/commands/xgettext.ts
+++ packages/tools/src/commands/xgettext.ts
@@ -596,12 +596,12 @@ export class XGettext implements Command {
 			filename = this.options.output;
 		}
 
-		const outputDir =
+		const outputFilename =
 			typeof this.options.outputDir === 'undefined'
-				? ''
-				: this.options.outputDir;
+				? filename
+				: `${this.options.outputDir}/${filename}`;
 
-		writeFileSync(path.join(outputDir, filename), po);
+		writeFileSync(outputFilename, po);
 	}
 
 	private fillExclusionCatalog(catalogs: Array<string>): boolean {
